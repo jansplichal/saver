084075f8258dd4dcd1efdcbaa033f9db2d8b3413
Jan Splichal <yansplichal@gmail.com>
2015-07-02 22:24:33 +0200

    fizes

--- routes/myexpenses.js
+++ routes/myexpenses.js
@@ -9,28 +9,27 @@
   DELETE  /:id              ->  destroy
 */
 module.exports = function(router){
+  router.get('/', function *(next) {
+    this.redirect('myexpenses/day');
+  });/*jshint ignore:line*/
 
   router.get('/day', function *(next) {
     yield this.render('myexpenses/day',{
-      pagename: 'My first page'
     });
   });
 
   router.get('/week', function *(next) {
     yield this.render('myexpenses/week',{
-      pagename: 'My first page'
     });
   });
 
   router.get('/month', function *(next) {
     yield this.render('myexpenses/month',{
-      pagename: 'My first page'
     });
   });
 
   router.get('/year', function *(next) {
     yield this.render('myexpenses/year',{
-      pagename: 'My first page'
     });
   });
 
